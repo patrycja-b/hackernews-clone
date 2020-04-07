a4bda45fa541a1dd927760ec2c56a46ab29f81ad
Patrycja <patrycja.batko@gmail.com>
2020-04-07 13:23:13 +0200

    Add query for the feed

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,39 @@
 const { GraphQLServer } = require("graphql-yoga");
 
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL",
+  },
+];
+
 // define graphQl schema
 const typeDefs = `
 type Query {
   info: String!
+  feed: [Link]!
 }
+
+type Link {
+    id: ID!
+    description: String!
+    url: String!
+  }
 `;
 
 // 2 add resolver for field
 const resolvers = {
   Query: {
-    info: () => "api info",
+    info: () => "This is the API of a Hackernews Clone",
+    feed: () => links,
+  },
+
+  // Parent is result of parent resolver -> one elemenf from links list in this case, this implementation can be omitted
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
   },
 };
 
